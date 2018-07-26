4f53837427c6e9593eec7aaca484566dc2e717b8
maxime-manicourt <maxime.manicourt@gmail.com>
2018-07-26 02:05:58 +0200

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do |index|
+  user = User.create(email: Faker::Internet.email)
+end
+
+10.times do |index|
+  a = rand(1..10)
+  link = Link.create(link: Faker::Internet.url, user_id: a)
+end
+
+10.times do |index|
+  a = rand(1..10)
+  b = rand(1..10)
+  comment = Comment.create(content: Faker::Friends.quote, user_id: a, links_id: b)
+end
+
+10.times do |index|
+  a = rand(1..10)
+  b = rand(1..10)
+  subcomment = Subcomment.create(content: Faker::Friends.character, user_id: a, comment_id: b)
+end
